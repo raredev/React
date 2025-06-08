94848149c59ce25d65d13b0b2271fc9317925602
raredev <rahulreddev@gmail.com>
2025-06-08 19:41:49 +0530

    [IMP] react essentials: Using javascript expression with jsx

--- React-Essentials/src/App.jsx
+++ React-Essentials/src/App.jsx
@@ -1,8 +1,26 @@
+import reactImg from "./assets/react-core-concepts.png"; // Importing Images for optimization
+
+const keywordItems = ["Fundamental", "Core", "Basic"];
+
+function Header() {
+  const keywordItem =
+    keywordItems[Math.floor(Math.random() * keywordItems.length)]; // Outputting dynamic value
+  return (
+    <header>
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>React Basic + Essentials</h1>
+      <p>
+        {keywordItem} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
+
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
-      <p>Understanding React Components</p>
+      <Header />
     </div>
   );
 }
