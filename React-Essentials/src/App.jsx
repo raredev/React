c199dbcc515e46665313492ee50959801f5a2687
raredev <rahulreddev@gmail.com>
2025-06-09 22:25:35 +0530

    [IMP] react-essentials: outputing list data dynamically using map method

--- React-Essentials/src/App.jsx
+++ React-Essentials/src/App.jsx
@@ -30,18 +30,13 @@ function App() {
     <div>
       <Header />
       <main>
+        {/* {[<p>Hello</p>,<p>World</p>]} > We can render list item directly */}
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept
-              title={CORE_CONCEPTS[0].title}
-              description={CORE_CONCEPTS[0].description}
-              image={CORE_CONCEPTS[0].image}
-            />
-            {/* Or Alternatively if object has same key that matches the props */}
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            {CORE_CONCEPTS.map((item) => (
+              <CoreConcept key={item.title} {...item} />
+            ))}
           </ul>
         </section>
         <section id="examples">
