6b40538bf0c314fa64898118c7b25bd4d542517a
raredev <rahulreddev@gmail.com>
2025-06-15 12:22:30 +0530

    [IMP] react-essentials-dd: use react built in method createElement to render components

--- React-Essentials/src/index.jsx
+++ React-Essentials/src/index.jsx
@@ -1,7 +1,11 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App.jsx";
 import "./index.css";
 
 const entryPoint = document.getElementById("root");
-ReactDOM.createRoot(entryPoint).render(<App />);
+// ReactDOM.createRoot(entryPoint).render(<App />);
+// We can also use react built in methods to create element instead of
+// relying on built in process
+ReactDOM.createRoot(entryPoint).render(React.createElement(App));
