6cb5c6a9a47259634da31a0d113bf77d234f8bc4
raredev <rahulreddev@gmail.com>
2025-08-24 18:21:01 +0530

    [IMP] react-styled-components: making styled components reusable by defining in a separate class and combining multiple styled components

---
+++ Styling-React-Components/src/components/Input.jsx
@@ -0,0 +1,32 @@
+import { styled } from "styled-components";
+
+const Label = styled.label`
+  display: block;
+  margin-bottom: 0.5rem;
+  font-size: 0.75rem;
+  font-weight: 700;
+  letter-spacing: 0.1em;
+  text-transform: uppercase;
+  color: ${({ $isInvalid }) => ($isInvalid ? "#f87171" : "#6b7280")};
+`;
+
+const Input = styled.input`
+  width: 100%;
+  padding: 0.75rem 1rem;
+  line-height: 1.5;
+  background-color: ${({ $isInvalid }) => ($isInvalid ? "#fed2d2" : "#d1d5db")};
+  color: ${({ $isInvalid }) => ($isInvalid ? "#ef4444" : "#374151")};
+  border: 1px solid
+    ${({ $isInvalid }) => ($isInvalid ? "#f73f3f" : "transparent")};
+  border-radius: 0.25rem;
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+`;
+
+export default function CustomInput({ label, isInvalid, ...props }) {
+  return (
+    <p>
+      <Label $isInvalid={isInvalid}>{label}</Label>
+      <Input $isInvalid={isInvalid} {...props} />
+    </p>
+  );
+}
