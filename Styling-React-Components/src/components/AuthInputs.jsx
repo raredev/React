52f82f535da5f6d6a681ba8755d8af0ff1ed21e3
raredev <rahulreddev@gmail.com>
2025-08-24 14:56:44 +0530

    [IMP] react-styled-components: best practice to use dollar sign to avoid name clash

--- Styling-React-Components/src/components/AuthInputs.jsx
+++ Styling-React-Components/src/components/AuthInputs.jsx
@@ -15,17 +15,17 @@ const Label = styled.label`
   font-weight: 700;
   letter-spacing: 0.1em;
   text-transform: uppercase;
-  color: ${({ isInvalid }) => (isInvalid ? "#f87171" : "#6b7280")};
+  color: ${({ $isInvalid }) => ($isInvalid ? "#f87171" : "#6b7280")};
 `;
 
 const Input = styled.input`
   width: 100%;
   padding: 0.75rem 1rem;
   line-height: 1.5;
-  background-color: ${({ isInvalid }) => (isInvalid ? "#fed2d2" : "#d1d5db")};
-  color: ${({ isInvalid }) => (isInvalid ? "#ef4444" : "#374151")};
+  background-color: ${({ $isInvalid }) => ($isInvalid ? "#fed2d2" : "#d1d5db")};
+  color: ${({ $isInvalid }) => ($isInvalid ? "#ef4444" : "#374151")};
   border: 1px solid
-    ${({ isInvalid }) => (isInvalid ? "#f73f3f" : "transparent")};
+    ${({ $isInvalid }) => ($isInvalid ? "#f73f3f" : "transparent")};
   border-radius: 0.25rem;
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
 `;
@@ -54,19 +54,19 @@ export default function AuthInputs() {
     <div id="auth-inputs">
       <ControlContainer>
         <p>
-          <Label isInvalid={emailNotValid}>Email</Label>
+          <Label $isInvalid={emailNotValid}>Email</Label>
           <Input
             type="email"
             // style={{ backgroundColor: emailNotValid ? "blue" : "white" }}
-            isInvalid={emailNotValid}
+            $isInvalid={emailNotValid}
             onChange={(event) => handleInputChange("email", event.target.value)}
           />
         </p>
         <p>
-          <Label isInvalid={passwordNotValid}>Password</Label>
+          <Label $isInvalid={passwordNotValid}>Password</Label>
           <Input
             type="password"
-            isInvalid={passwordNotValid}
+            $isInvalid={passwordNotValid}
             onChange={(event) =>
               handleInputChange("password", event.target.value)
             }
