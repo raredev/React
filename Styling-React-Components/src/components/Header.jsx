812c509de0943dff5969f64273c5833212965a4c
raredev <rahulreddev@gmail.com>
2025-08-25 21:59:50 +0530

    [IMP] react-tailwind-css: adding custom styles through configuration

--- Styling-React-Components/src/components/Header.jsx
+++ Styling-React-Components/src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className="flex flex-col items-center mt-8 mb-16">
       <img src={logo} alt="A canvas" className="w-52 h-52 object-contain" />
-      <h1 className="uppercase font-semibold text-3xl mt-2 text-amber-800 tracking-widest">
+      <h1 className="uppercase font-semibold text-3xl mt-2 text-amber-800 tracking-widest font-title">
         ReactArt
       </h1>
       <p>A community of artists and art-lovers.</p>
