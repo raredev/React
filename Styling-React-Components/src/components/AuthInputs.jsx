010d1538067b32ef99d3c44808019d1a44461583
raredev <rahulreddev@gmail.com>
2025-08-26 22:13:36 +0530

    [IMP] react-tailwind-css: migrating styles to tailwind css

--- Styling-React-Components/src/components/AuthInputs.jsx
+++ Styling-React-Components/src/components/AuthInputs.jsx
@@ -31,8 +31,11 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ControlContainer>
+    <div
+      id="auth-inputs"
+      className="flex flex-col mb-3 md:w-5/12 shadow-md bg-gradient-to-b from-stone-700 to-stone-800 mx-auto px-10 pt-10 pb-5 rounded"
+    >
+      <div className="flex flex-col gap-5 mb-5">
         <Input
           label="Email"
           type="email"
@@ -47,9 +50,9 @@ export default function AuthInputs() {
             handleInputChange("password", event.target.value)
           }
         />
-      </ControlContainer>
-      <div className="actions">
-        <button type="button" className="text-button">
+      </div>
+      <div className="flex justify-end gap-4">
+        <button type="button" className="text-amber-400 hover:text-amber-500">
           <p className="paragraph">Create a new account</p>
         </button>
         <Button onClick={handleLogin}>Sign In</Button>
--- Styling-React-Components/src/components/Button.jsx
+++ Styling-React-Components/src/components/Button.jsx
@@ -1,7 +1,7 @@
 export default function Button({ children, ...props }) {
   return (
     <button
-      className="px-3 py-2 font-semibold uppercase rounded text-stone-800 hover:bg-amber-600"
+      className="px-3 py-2 font-semibold uppercase rounded text-stone-800 bg-amber-600 hover:bg-amber-500"
       {...props}
     >
       {children}
