cb883625d97f339b7a4b72f83f2dd06898a82b93
raredev <rahulreddev@gmail.com>
2025-08-26 21:52:51 +0530

    [IMP] react-tailwind-css: adding dynamic conditional styles

--- Styling-React-Components/src/components/Input.jsx
+++ Styling-React-Components/src/components/Input.jsx
@@ -1,32 +1,17 @@
-import { styled } from "styled-components";
-
-const Label = styled.label`
-  display: block;
-  margin-bottom: 0.5rem;
-  font-size: 0.75rem;
-  font-weight: 700;
-  letter-spacing: 0.1em;
-  text-transform: uppercase;
-  color: ${({ $isInvalid }) => ($isInvalid ? "#f87171" : "#6b7280")};
-`;
-
-const Input = styled.input`
-  width: 100%;
-  padding: 0.75rem 1rem;
-  line-height: 1.5;
-  background-color: ${({ $isInvalid }) => ($isInvalid ? "#fed2d2" : "#d1d5db")};
-  color: ${({ $isInvalid }) => ($isInvalid ? "#ef4444" : "#374151")};
-  border: 1px solid
-    ${({ $isInvalid }) => ($isInvalid ? "#f73f3f" : "transparent")};
-  border-radius: 0.25rem;
-  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-`;
-
-export default function CustomInput({ label, isInvalid, ...props }) {
+export default function Input({ label, isInvalid, ...props }) {
+  let labelClass = "block mb-1 text-xs font-bold tracking-wide uppercase ";
+  let inputClass = "w-full px-3 py-2 leading-tight border rounded shadow";
+  if (isInvalid) {
+    labelClass += "text-red-600";
+    inputClass += "bg-red-600 text-red-700 border-red-300";
+  } else {
+    labelClass += "text-stone-300";
+    inputClass += "bg-stone-300 text-gray-700";
+  }
   return (
     <p>
-      <Label $isInvalid={isInvalid}>{label}</Label>
-      <Input $isInvalid={isInvalid} {...props} />
+      <label className={labelClass}>{label}</label>
+      <input className={inputClass} {...props} />
     </p>
   );
 }
